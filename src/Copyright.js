d0cb2340ca147f8e92ca2520b26354611a185a48
Dobrotvorsky Fedor Lvovich <fdobrotv@gradoservice.ru>
2020-05-31 19:35:16 +0600

    [NF] Copyright.js added

--- src/Copyright.js
+++ src/Copyright.js
@@ -6,8 +6,8 @@ export default function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <MuiLink color="inherit" href="https://material-ui.com/">
-        Your Website
+      <MuiLink color="inherit" href="https://fdobrotv.com/">
+        Fedor Dobrotvorskii Website
       </MuiLink>{' '}
       {new Date().getFullYear()}
       {'.'}
